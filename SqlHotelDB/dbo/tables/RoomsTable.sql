4b9a32f27b577425889867c79de9b6b8ce243b98
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-17 23:43:18 -0500

    Add project files.

---
+++ SqlHotelDB/dbo/tables/RoomsTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RoomsTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoomTypeId] INT NOT NULL, 
+    [RoomNumber] VARCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_RoomsTable_RoomTypeTable] FOREIGN KEY (RoomTypeId) REFERENCES RoomTypeTable(Id)
+)
