3973ad3ff3d8411e2ac0ce494ef84420a8e12bdd
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-20 00:11:28 -0500

    created a loadData method using IDbConnection, Dapper, and SqlClient

---
+++ HotelManagementLibrary/DataBases/SqlDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary.DataBases
+{
+    public class SqlDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatement, U parameter, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameter).ToList();
+                return rows;
+            }
+        }
+    }
+}
