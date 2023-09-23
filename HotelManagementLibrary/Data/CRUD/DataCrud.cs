d3f14bb6c30cd96b96dcbdc93bed168fd801e33b
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-22 22:52:33 -0500

    Used dependancy injection requiring  DataAccess parameter to get passed into the instantiation of a CRUD object.

---
+++ HotelManagementLibrary/Data/CRUD/DataCrud.cs
@@ -0,0 +1,19 @@
+﻿using HotelManagementLibrary.DataBases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary.Data.CRUD
+{
+    public class DataCrud
+    {
+        
+        private readonly SqlDataAccess db;
+        public DataCrud(SqlDataAccess connectionString)
+        {
+            db = connectionString;
+        }
+    }
+}
