6a958fb19d8ac10a496b0c1796bdbe5609b7ddfd
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-24 22:58:19 -0500

    learning to use SQL stored procedures. Used startDate and endDate in conditional statements to query available rooms

---
+++ SqlHotelDB/dbo/spRoomTypeTable_GetAvailableTypes.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[spRoomTypeTable_GetAvailableTypes]
+	@startDate date,
+	@endDate date
+AS
+Begin
+	set nocount on;
+
+	select rt.Id, rt.Title, rt.Description, rt.Price
+from dbo.RoomsTable r
+inner join dbo.RoomTypeTable rt on r.RoomTypeId = rt.Id
+where r.Id not in (
+
+--selects the RoomId that is interefered by the new booking(dates)
+select b.RoomId
+from dbo.BookingsTable b
+where (@startDate >= b.StartDate and @endDate <= b.EndDate) or
+		(@startDate <= b.StartDate and @endDate >= b.EndDate) or
+		(@startDate <= b.StartDate and (@endDate >= b.StartDate and @endDate <= b.EndDate) or
+		((@startDate >= b.StartDate and @startDate <= b.EndDate) and @endDate >= b.EndDate))
+
+		)
+
+group by rt.Id, rt.Title, rt.Description, rt.Price
+End
