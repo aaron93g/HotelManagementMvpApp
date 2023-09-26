109a15847582f93a93265efa91f8f4d7623f37ae
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-26 01:06:12 -0500

    builds on an existing stored procedure which gets available rooms for a specific date using conditionals to determing if the new start and end dates interfere with already booked dates.
    This SP adds the option to look for a specific room option for specified dates using 2 WHERE criteria.

---
+++ SqlHotelDB/dbo/Stored Procedures/spRooms_GetAvailableRooms.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[spRooms_GetAvailableRooms]
+	@startDate date,
+	@endDate date,
+	@roomTypeId int
+AS
+
+begin
+	
+	select r.*
+	from dbo.RoomsTable r
+	inner join dbo.RoomTypeTable rt on r.RoomTypeId = rt.Id
+	where r.RoomTypeId = @roomTypeId and
+	r.Id not in 
+	(
+
+		--selects the RoomId that is interefered by the new booking(dates)
+		select b.RoomId
+		from dbo.BookingsTable b
+		where (@startDate >= b.StartDate and @endDate <= b.EndDate) or
+				(@startDate <= b.StartDate and @endDate >= b.EndDate) or
+				(@startDate <= b.StartDate and (@endDate >= b.StartDate and @endDate <= b.EndDate) or
+				((@startDate >= b.StartDate and @startDate <= b.EndDate) and @endDate >= b.EndDate))
+
+	)
+
+end
