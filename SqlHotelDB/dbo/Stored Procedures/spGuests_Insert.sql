d345e61054257393e884b76e2ae4f98e3de33e80
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-26 00:58:28 -0500

    used stored procedure to evaluate a condition. if no guest with the same name as the passed in parameter we insert and create new guest else we return the existing guest

---
+++ SqlHotelDB/dbo/Stored Procedures/spGuests_Insert.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[spGuests_Insert]
+	@firstName nvarchar(20),
+	@lastName nvarchar(20)	
+AS
+begin
+
+	set nocount on;
+
+	if not exists(select 1 from dbo.GuestsTable where FirstName = @firstName and LastName = @lastName)
+	begin
+		insert into dbo.GuestsTable (FirstName, LastName)
+		values (@firstName, @lastName)
+	end
+
+	select top 1 [Id], [FirstName], [LastName]
+	from dbo.GuestsTable
+	where FirstName = @firstName and LastName = @lastName
+end
