63a4ba5d729d33a4cc4df332aeddb325ab537441
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-26 00:54:16 -0500

    Made a create class for adding reservation/bookings. practiced sophisticated stored procedures, passing in parameters and returning values from the data base

---
+++ HotelManagementLibrary/Data/SqlData/Create.cs
@@ -0,0 +1,54 @@
+﻿using HotelManagementLibrary.DataBases;
+using HotelManagementLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary.Data.SqlData
+{
+    public class Create
+    {
+        string connection = "Default";
+        private readonly ISqlDataAccess _db;
+        public Create(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public void Booking(string firstName,
+                            string lastName,
+                            DateTime startDate,
+                            DateTime endDate,
+                            int roomTypeId)
+        {
+            GuestModel guest = _db.LoadData<GuestModel, dynamic>("spGuests_Insert",
+                                                                 new { firstName, lastName},
+                                                                 connection,
+                                                                 true).First();
+
+            RoomTypeModel roomType = _db.LoadData<RoomTypeModel, dynamic>("select * from dbo.RoomTypeTable where Id = @roomTypeId",
+                                                                          new { roomTypeId },
+                                                                          connection).First();
+
+            TimeSpan stayDuration = endDate.Subtract(startDate);
+
+            List<RoomModel> availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.spRooms_GetAvailableRooms",
+                                                                              new { startDate, endDate, roomTypeId},
+                                                                              connection,
+                                                                              true);
+
+            _db.SaveData("dbo.spBookings_Insert",
+                         new { roomId = availableRooms.First().RoomId, 
+                             guestId = guest.GuestId, 
+                             startDate = startDate,
+                             endDate = endDate,
+                             totalCost = stayDuration.Days * roomType.RoomTypePrice},
+                         connection,
+                         true);
+        }
+
+
+    }
+}
