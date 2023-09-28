8c6fff26379df066b84ae409a100ac86c45596a8
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-28 00:16:45 -0500

    Created a stored procedure that gets information joinning multiple tables and passes c# arguements to filter specific data.

---
+++ SqlHotelDB/dbo/Stored Procedures/spBookings_FindReservation.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_FindReservation]
+
+@todaysDate date,
+@firstName nvarchar(20),
+@lastName nvarchar(20)
+
+AS
+BEGIN
+	
+	select r.RoomNumber, rt.Title, g.FirstName, g.LastName, b.StartDate, b.EndDate, b.CheckedIn, rt.Price, b.TotalCost
+from dbo.BookingsTable b
+inner join dbo.GuestsTable g on g.Id = b.GuestId
+inner join dbo.RoomsTable r on r.Id = b.RoomId
+inner join dbo.RoomTypeTable rt on rt.Id = r.RoomTypeId
+where b.StartDate = @todaysDate and g.FirstName = @firstName and g.LastName = @lastName
+
+END
