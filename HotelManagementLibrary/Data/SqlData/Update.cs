af490a635e0c30c72322252b087401baf3274837
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-28 14:17:34 -0500

    Added Update class with a method to update the Check in column in bookingsTable.

---
+++ HotelManagementLibrary/Data/SqlData/Update.cs
@@ -0,0 +1,28 @@
+﻿using HotelManagementLibrary.DataBases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary.Data.SqlData
+{
+    public class Update
+    {
+        string connection = "Default";
+        private readonly ISqlDataAccess _db;
+        public Update(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public void CheckIn(int bookingId)
+        {
+
+            _db.SaveData("spBookings_CheckIn",
+                        new {bookingId, checkingIn = 1},
+                        connection,
+                        true);
+        }
+    }
+}
