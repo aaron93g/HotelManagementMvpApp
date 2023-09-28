40dd51d23a97a9055f2519930934c443df9e82e7
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-28 00:18:14 -0500

    created a class with properties to store data from the FindReservation SP

---
+++ HotelManagementLibrary/Models/SearchedReservations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementLibrary.Models
+{
+    public class SearchedReservations
+    {
+        public string ReservationRoomNumber { get; set; }
+        public string ReservationRoomTitle { get; set; }
+        public string ReservationFirstName { get; set; }
+        public string ReservationLastName { get; set; }
+        public DateOnly ReservationStartDate { get; set; }
+        public DateOnly ReservationEndDate { get; set;}
+        public bool ReservationCheckedIn { get; set; }
+        public decimal PricePerDay { get; set; }
+        public decimal ReservationTotalCost { get; set; }
+    }
+}
