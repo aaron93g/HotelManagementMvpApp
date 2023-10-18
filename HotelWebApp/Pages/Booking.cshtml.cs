2c7fb8a4cca8795ef71c60b5ee2320ea941d1858
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-10-18 01:58:04 -0500

    created properties which are routed from the welcome page to be used in the confirmation page

--- HotelWebApp/Pages/Booking.cshtml.cs
+++ HotelWebApp/Pages/Booking.cshtml.cs
@@ -5,6 +5,19 @@ namespace HotelWebApp.Pages
 {
     public class BookingModel : PageModel
     {
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int RoomType1Choice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int RoomType2Choice { get; set; }
+
         public void OnGet()
         {
         }
